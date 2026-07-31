63943c9bbf4684892c59d20d8745f5d03619197c
rishurajgit <rajrishu.cs@gmail.com>
2026-07-31 08:29:52 +0530

    Changed the AI Model

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    GROQ_API_KEY: str
-    GROQ_MODEL: str
+    # GROQ_API_KEY: str
+    # GROQ_MODEL: str
     VET_API_BASE_URL: str
     VET_API_TOKEN: str
+    
+    GEMINI_API_KEY: str
+    GEMINI_MODEL: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
--- app/core/llm.py
+++ app/core/llm.py
@@ -1,10 +1,13 @@
 from langchain_groq import ChatGroq
-
+from langchain_google_genai import ChatGoogleGenerativeAI 
 from app.core.config import settings
 
 
-llm = ChatGroq(
-    model=settings.GROQ_MODEL,
-    api_key=settings.GROQ_API_KEY,
+llm = ChatGoogleGenerativeAI(
+    # model=settings.GROQ_MODEL,
+    # api_key=settings.GROQ_API_KEY,
+    
+    model=settings.GEMINI_MODEL,
+    api_key=settings.GEMINI_API_KEY,
     temperature=0,
 )
